71da92920c85b714fcb17a44b3341424dd88b3be
conno <cmcd595@student.ubc.ca>
2023-11-28 15:50:12 -0500

    Metal Detector Added

--- src/main/java/net/solaris595/tutorialmod/item/ModItems.java
+++ src/main/java/net/solaris595/tutorialmod/item/ModItems.java
@@ -9,14 +9,18 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.solaris595.tutorialmod.TutorialMod;
+import net.solaris595.tutorialmod.item.custom.MetalDetectorItem;
 
 public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
 
+    public static final Item METAL_DETECTOR = registerItem("metal_detector", new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
+
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
         entries.add(RAW_RUBY);
+        entries.add(METAL_DETECTOR);
     }
 
     private static Item registerItem(String name, Item item) {
